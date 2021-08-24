4f3a345d6f60c580b8c245a4ea284c8c27899ab3
Azizjon Nigmatjonov <74749706+AzizjonNigmatjonov@users.noreply.github.com>
2021-08-24 12:23:02 +0500

    updated for more things

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,5 +7,35 @@ for (let i = 0; i < navItemLength; i++) {
   }
 }
 
-gsap.from('.anim1', { opacity: 0, duration: 1, y: -100, stagger: 0.6 })
-gsap.from('.main-content-bottom', { duration: 2, y: -200, opacity: 0})
+var a = 0;
+$(window).scroll(function() {
+    
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function() {
+            var $this = $(this),
+            countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+            
+            {
+                
+                duration: 2000,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+                
+            });
+        });
+        a = 1;
+    }
+    
+});
